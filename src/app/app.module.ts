c5c2f83b49b080f895f87b836cf79f61db67bf95
maxkha-3 <maxkha-3@student.ltu.se>
2018-10-04 01:19:02 +0200

    Added a DashboardComponent and a router outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
+//Router path template
+const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent }];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
